118ca5db8d0e42c82fa1e40e6014f7a43970740b
Sam Davies <sdavies@shinobicontrols.com>
2014-05-01 10:42:55 +0100

    Initial commit of PopGoesShinobi project
    Simple single-view application with liftoff applied

---
+++ PopGoesShinobi/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  PopGoesShinobi
+//
+//  Created by Sam Davies on 01/05/2014.
+//  Copyright (c) 2014 Shinobi Controls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PopGoesShinobi/SCViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCViewController.h
+//  PopGoesShinobi
+//
+//  Created by Sam Davies on 01/05/2014.
+//  Copyright (c) 2014 Shinobi Controls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCViewController : UIViewController
+
+@end
